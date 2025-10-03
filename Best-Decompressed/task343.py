041fec3d0ad1e840a2eecf4ee58b2dd77148115c
russellchris500 <russellchris0@gmail.com>
2025-10-03 11:22:35 -0400

    tasks 31 and 343

--- Best-Decompressed/task343.py
+++ Best-Decompressed/task343.py
@@ -1,1 +1,1 @@
-p=lambda g:[[*zip(*d[:15])]for l in(3,4,6,8)if(d:=(h:=[*zip(*g)])[:l]*6)[:(e:=g[4].index(0))]==h[:e]][0]
+p=lambda g:[(r[:6+2*(r[:4]in(r[4:8],r[8:12]))]*3)[:15]for r in g]
--- Work/task031.py
+++ Work/task031.py
@@ -1,4 +1,7 @@
 #p=lambda g:[*zip(*filter(any,zip(*filter(any,g))))]
 #p=lambda g,f=filter:[*zip(*f(any,zip(*f(any,g))))]
 #p=lambda g:[*filter(any,zip(*filter(any,zip(*g))))]
-p=lambda g:[eval("*filter(any,zip("*2+"*g))))")]
+# p=lambda g:[eval("*filter(any,zip("*2+"*g))))")]
+#p=lambda g,c=2:[r for r in g if any(r)]
+p=lambda g,c=2:[*zip(*filter(any,g))]
+#p=lambda g,c=2:[g,p([*filter(any,zip(*g))],c-1)][c>0]
