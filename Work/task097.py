24704a71a3d1843a485256ae92df2efc6035461f
Chris Russell <russellchris0@gmail.com>
2025-10-24 17:19:49 -0400

    97

--- Work/task097.py
+++ Work/task097.py
@@ -1,11 +1,2 @@
-# p=lambda g,E=enumerate:[[v*(sum(len(g)>y+i>=0<=x+j<len(r)>0<g[y+i][x+j]for i in(-1,0,1)for j in(-1,0,1))>1)for x,v in E(r)]for y,r in E(g)]
-# def p(g):
-#  print("")
-#  for r in zip((e:=[[0]*30])+g,g,g[1:]+e):
-#   for p in zip((f:=[[0]*3])+(z:=[*zip(*r)]),z,z[1:]+f):
-#    print(p)
-#   print("")
-#  return g
-# p=lambda g:[[sum(p,[]).count(0)<8and p[1][1]for p in zip(z,z[1:],z[2:])]for r in zip([[0]*30]+g,g,g[1:]+[[0]*30])if(z:=[[0]*3,*zip(*r),[0]*3])]
-# import re
-# p=lambda g:eval(re.sub(f"(<=0, 0, 0{{{len(g[0])}}}0, )","0",str(g)))
+# p=lambda u,k=11,s=30*[0]:-k*u or p([[k and p|16*((a|b)%16>0)or p%16*(p>15)for p,a,b in zip(r,s,[0]+s)if[s:=r]]for*r,in zip(*u[::-1])],~-k)
+p=lambda u,k=11,s=30*[0]:-k*u or p([[k and p|16*((a|b)>0)or (p>>4)%16for p,a,b in zip(r,s,[0]+s)if[s:=r]]for*r,in zip(*u[::-1])],~-k)
