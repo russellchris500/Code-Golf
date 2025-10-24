d6fd5cba7a2eaa389d8fb60a0384c04b3159ecdd
emilyfairfield <russellemily13@gmail.com>
2025-10-24 09:38:16 -0400

    removed unnecessary deep copies

--- Best_Decompressed/task206.py
+++ Best_Decompressed/task206.py
@@ -1,8 +1,8 @@
 def p(g,R=range):
- h,w=len(g),len(g[0]);o=[r[:]for r in g];P=b=0
+ h,w=len(g),len(g[0]);P=b=0
  for i in R(h-2):
   for j in R(w-2):c=sum(g[r][c]not in[0,5]for r in R(i,i+3)for c in R(j,j+3));c>b and(P:=[[g[r][c]for c in R(j,j+3)]for r in R(i,i+3)],b:=c)
  for i in R(h):
   for j in R(w):
-   if g[i][j]==5:[P*(0<=n<h)*(0<=m<w)and o[n].__setitem__(m,P[d+1][e+1])for d in R(-1,2)for e in R(-1,2)for n,m in[(i+d,j+e)]];break
- return o
+   if g[i][j]==5:[P*(0<=n<h)*(0<=m<w)and g[n].__setitem__(m,P[d+1][e+1])for d in R(-1,2)for e in R(-1,2)for n,m in[(i+d,j+e)]];break
+ return g
