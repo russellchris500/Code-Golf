a70c4f0272b986a07b9d1739beeb913546cc9a85
Chris Russell <russellchris0@gmail.com>
2025-10-26 15:33:21 -0400

    126,166,303

--- Best_Decompressed/task126.py
+++ Best_Decompressed/task126.py
@@ -1,6 +1,1 @@
-def p(r):
- p=[u[:]for u in r];t,s=len(r),len(r[0])
- for q in range(1,t):
-  for u in range(1,s-1):
-   if r[q][u]==0and r[q][u-1]and r[q][u+1]and r[q][u-1]==r[q][u+1]and r[q-1][u]==r[q][u-1]:p[t-1][u]=4
- return p
+p=lambda g:g[:-1]+[[(len([*filter(None,c)])==1)*4for p,c in zip(g[-1],zip(*g))]]
--- Best_Decompressed/task303.py
+++ Best_Decompressed/task303.py
@@ -1,8 +1,1 @@
-def p(j,A=range):
-	C,D=len(j),len(j[0])
-	for B in A(C):
-		if sum(j[B])==0:j[B]=[2]*D
-	for E in A(D):
-		if all(j[A][E]in[0,2]for A in A(C)):
-			for B in A(C):j[B][E]=2
-	return j
+p=lambda g:[[p or({*c}&{*r}=={0})*2for p,c in zip(r,zip(*g))]for r in g]
