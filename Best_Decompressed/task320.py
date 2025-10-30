707f9a7f71ed49689c817ed4341a4ad2e1488250
Chris Russell <russellchris0@gmail.com>
2025-10-30 18:59:54 -0400

    320

--- Best_Decompressed/task320.py
+++ Best_Decompressed/task320.py
@@ -1,6 +1,1 @@
-def p(j,A=range):
-	E=len(j);F=len(j[0]);B=[A[:]for A in j]
-	for C in A(F):
-		D=[A for A in A(E)if j[A][C]];G=len(D)//2
-		for H in A(G):B[D[-1-H]][C]=8
-	return B
+p=lambda g:[*zip(*[[(r.count(2)//2>=len(r)-x)*8or r[x]for x in range(len(r))]for r in zip(*g)])]
