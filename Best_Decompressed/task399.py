958ab4bd9609f492cf3b02c516a1c732bf9285c0
Chris Russell <russellchris0@gmail.com>
2025-10-30 18:42:40 -0400

    399

--- Best_Decompressed/task399.py
+++ Best_Decompressed/task399.py
@@ -1,4 +1,1 @@
-def p(i,r=range):
- l=sum(i[d][h]==i[d][h+1]==i[d+1][h]==i[d+1][h+1]==2for d in r(len(i)-1)for h in r(len(i)-1));d=[[0]*3for d in'***']
- for h in r(l):d[h*2//3][h*2%3]=1
- return d
+p=lambda g,R=(0,1,2):[[(l%2)*(l<sum(sum(g,[]))//4)for x in R if[l:=y*3+x+1]]for y in R]
--- Private-Uncompressed/task139.py
+++
@@ -1,5 +0,0 @@
-def p(g):
- F=[(i//9,i%9)for i in range(81)if g[i//9][i%9]==4]
- for T in[[(i,j)for i,j in F if i+j<8],[(i,j)for i,j in F if i+j>=8]]:
-  if T:I,J=zip(*T);[[g[i].__setitem__(j,7)for j in range(min(J),max(J)+1)if g[i][j]<1]for i in range(min(I),max(I)+1)]
- return g
