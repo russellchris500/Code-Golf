261b188e6cb59a3ddc072f5065c56339bb4f9907
Chris Russell <russellchris0@gmail.com>
2025-10-30 18:52:48 -0400

    149

--- Best_Decompressed/task149.py
+++ Best_Decompressed/task149.py
@@ -1,11 +1,1 @@
-def p(p):
- n=range
- d=[[0]*3for r in n(3)]
- for r in n(3):
-  for a in n(3):
-   t=0
-   for c in n(3):
-    for l in n(3):
-     if p[r*4+c][a*4+l]==6:t+=1
-   d[r][a]=1if t>=2else 0
- return d
+p=lambda g,R=(0,4,8):[[sum(sum([r[x:x+3]for r in g[y:y+3]],[]))>6for x in R]for y in R]
